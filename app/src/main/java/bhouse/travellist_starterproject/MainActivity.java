d3db818617156191fa24be26dbe191b2e638d3d7
Serguei Outenkov <sergueioutenkov@gmail.com>
2015-08-27 17:21:04 -0300

    Initial commit

---
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -0,0 +1,88 @@
+package bhouse.travellist_starterproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+  private Menu menu;
+  private boolean isListView;
+  private TravelListAdapter mAdapter;
+
+  private RecyclerView mRecyclerView;
+  private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    isListView = true;
+
+    mRecyclerView = (RecyclerView) findViewById(R.id.list);
+    mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+    mRecyclerView.setLayoutManager(mStaggeredGridLayoutManager);
+
+    mAdapter = new TravelListAdapter(this);
+    mRecyclerView.setAdapter(mAdapter);
+
+    TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
+      @Override
+      public void onItemClick(View v, int position) {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+        startActivity(intent);
+      }
+    };
+
+    mAdapter.setOnItemClickListener(onItemClickListener);
+
+  }
+
+  private void setUpActionBar() {
+
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_main, menu);
+    this.menu = menu;
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.action_toggle) {
+      toggle();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void toggle() {
+    MenuItem item = menu.findItem(R.id.action_toggle);
+    if (isListView) {
+      mStaggeredGridLayoutManager.setSpanCount(2);
+      item.setIcon(R.drawable.ic_action_list);
+      item.setTitle("Show as list");
+      isListView = false;
+    } else {
+      mStaggeredGridLayoutManager.setSpanCount(1);
+      item.setIcon(R.drawable.ic_action_grid);
+      item.setTitle("Show as grid");
+      isListView = true;
+    }
+  }
+}
